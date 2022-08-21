f6c7ba1cb5e1f47a5c9d326cf9a24796f1ddd47f
piotr-mrocz <p.mrocz@vp.pl>
2022-08-21 11:53:49 +0200

    Basic configure Sieve processor

---
+++ Backend/WebApi/WebApi/Sieve/AplicationSieveProcessor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Options;
+using Sieve.Models;
+using Sieve.Services;
+using WebApi.Models.Entities;
+
+namespace WebApi.Sieve;
+
+public class AplicationSieveProcessor : SieveProcessor
+{
+    public AplicationSieveProcessor(IOptions<SieveOptions> options) : base(options)
+    {
+    }
+
+    // Sort and filtr
+    protected override SievePropertyMapper MapProperties(SievePropertyMapper mapper)
+    {
+        mapper.Property<Country>(c => c.Id)
+            .CanSort()
+            .CanFilter();
+
+        mapper.Property<Country>(c => c.CountryName)
+            .CanSort()
+            .CanFilter();
+
+        mapper.Property<Country>(c => c.CountryCode)
+            .CanSort()
+            .CanFilter();
+
+        mapper.Property<Country>(c => c.CapitalCity)
+            .CanSort()
+            .CanFilter();
+
+        mapper.Property<Country>(c => c.Continent)
+            .CanSort()
+            .CanFilter();
+
+        mapper.Property<Country>(c => c.Population)
+            .CanSort()
+            .CanFilter();
+
+        return mapper;
+    }
+}
